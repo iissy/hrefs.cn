ee6baee39b962fbfb52ac0182ebcc31955a13055
hemin <hemin@kdniao.cn>
2019-09-27 10:03:22 +0800

    add Authorize

--- hrefs.cn/Controllers/MainController.cs
+++ hrefs.cn/Controllers/MainController.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace hrefs.cn.Controllers
 {
+    [Authorize]
     public class MainController : Controller
     {
         private readonly ILogger<HomeController> _logger;
